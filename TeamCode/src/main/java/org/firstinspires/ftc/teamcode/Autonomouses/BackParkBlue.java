aab8190a4bb378972340ec6f9f36667108b00cff
Bob <adish2121@gmail.com>
2022-02-02 18:45:28 -0800

    something

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomouses/BackParkBlue.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.Autonomouses;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Baguette;
+import org.firstinspires.ftc.teamcode.util.MecanumDriveTranslationalPID;
+import org.firstinspires.ftc.teamcode.util.Pose2D;
+
+@Autonomous(group = "Comp", name = "back park blue")
+class BackParkBlue extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Baguette.initializeBaguette();
+
+        MecanumDriveTranslationalPID move = new MecanumDriveTranslationalPID();
+
+        waitForStart();
+        if (opModeIsActive()) {
+            move.tPID(new Pose2D(24, 0, 0));
+            wait(1000);
+
+            move.tPID(new Pose2D(0, 24, 0));
+            wait(1000);
+        }
+    }
+}
